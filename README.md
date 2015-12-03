3b4c768f093c2306e9084683f88510566a918cb4
Jason Dudash <thenut77@gmail.com>
2015-12-03 16:35:55 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# openshiftexamples-nodemongo
-Node.js and MongoDB example
+# Node.js and MongoDB example
+This example simply opens a conection to a mongoDB and then creates a websever with a few routes to test that the database connection worked.  Note that each time it starts it will add items to 2 collections in the database.  You can test with the following routes:
+> "[HOST]/" = Hello page
+> "[HOST]/test1" = data in collection 1
+> "[HOST]/test2" = data in colleciton 2
+> "[HOST]/test3" = valid route but to a non-existent collection call on the DB
---
+++ index.js
@@ -0,0 +1,55 @@
+// setup database connection
+var MongoClient = require('mongodb').MongoClient;
+var user = process.env.MONGODB_USER || 'dummy';
+var pass = process.env.MONGODB_PASSWORD || 'dummy';
+var dbip = process.env.MONGODB_SERVICE_HOST || '127.0.0.1';
+var dbport = process.env.MONGODB_SERVICE_PORT || 27017;
+var url = 'mongodb://'+user+':'+pass+'@'+dbip+'/'+dbport+'/sampledb'
+// uncomment to use without authentication
+//var url = 'mongodb://'+dbip+'/'+dbport+'/sampledb'
+console.log('attempting mongoDB connection to ' + url);
+MongoClient.connect(url, function(err, db) {
+   if (err) {
+      console.log('unable to connect to mongo database -' + err);
+   } else {
+      console.log('connected to mongo database, creating some dummy data...');
+      // stuff in dummy data
+      db.createCollection('test1', {strict:false}, function(err, collection) {
+         collection.insert({name:"name1",type:1}, function(err, records) { if (err) throw err;});
+      });
+      db.createCollection('test2', {strict:false}, function(err, collection) {
+         collection.insert({name:"name",type:2}, function(err, records) { if (err) throw err;});
+      });
+
+      // setup webserver
+      var express = require('express');
+      var app = express();
+      app.get('/', function(req, res) {
+         res.send('Hello MongoDB');
+      });
+      app.get('/test1', function(req, res) {
+         db.collection('test1').find().toArray(function(err, items){
+            console.log(items);
+            res.send('items in test 1: ' + items.length);
+         });
+      });
+      app.get('/test2', function(req, res) {
+         db.collection('test2').find().toArray(function(err, items){
+            console.log(items);
+            res.send('items in test 2: ' + items.length);
+         });
+      });
+      app.get('/test3', function(req, res) {
+         db.collection('test3').find().toArray(function(err, items){
+            console.log(items);
+            res.send('items in test 3: ' + items.length);
+         });
+      });
+      // start listening for client connections
+      var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+      var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+      app.listen(server_port, server_ip_address, function() {
+         console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+      });
+   }
+});
