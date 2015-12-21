260504f99c85f7dabcb65aeb30c8bb71c8e35fbb
Jason Dudash <jdudash@redhat.com>
2015-12-21 16:10:53 -0500

    fixing bug with database name, adding IP address tracking (request and service host) as the primary database information.  This will allow a demonstration showing load balancing.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var dbip = process.env.MONGODB_SERVICE_HOST || '127.0.0.1';
 var dbport = process.env.MONGODB_SERVICE_PORT || 27017;
 var dbcoll = process.env.MONGODB_DATABASE || 'sampledb';
 var db = null;
-var url = 'mongodb://'+user+':'+pass+'@'+dbip+'/'+dbport+'/'+dbcoll;
+var url = 'mongodb://'+user+':'+pass+'@'+dbip+':'+dbport+'/'+dbcoll;
 // uncomment to use without authentication
-//var url = 'mongodb://'+dbip+'/'+dbport+'/'+dbcoll
+//var url = 'mongodb://'+dbip+':'+dbport+'/'+dbcoll
 
 var mongoCreatedCollections = false;
 var initMongo = function(errcallback) {
@@ -20,14 +20,7 @@ var initMongo = function(errcallback) {
          return;
       } else {
          db = mydb;
-         console.log('connected to mongo database, creating some dummy data...');
-         // stuff in dummy data
-         db.createCollection('test1', {strict:false}, function(err, collection) {
-            collection.insert({name:"name1",type:1}, function(err, records) { if (err) throw err;});
-         });
-         db.createCollection('test2', {strict:false}, function(err, collection) {
-            collection.insert({name:"name",type:2}, function(err, records) { if (err) throw err;});
-         });
+         console.log('connected to mongo database...');
          mongoCreatedCollections = true;
       }
    });
@@ -38,28 +31,51 @@ var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
    console.log('GET request');
-   res.send('Hello MongoDB.\n <a href=\'/test1\'>test1</a> \n <a href=\'/test2\'>test2</a> \n <a href=\'/test3\'>test3</a>');
-});
-app.get('/test1', function(req, res) {
-   if (!mongoCreatedCollections) { res.send('test1 - no DB connection'); return; }
-   db.collection('test1').find().toArray(function(err, items){
-      console.log(items);
-      res.send('items in test 1: ' + items.length);
+
+   var requestip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
+   var os = require('os');
+   var interfaces = os.networkInterfaces();
+   var addresses = [];
+   for (var k in interfaces) {
+       for (var k2 in interfaces[k]) {
+           var address = interfaces[k][k2];
+           if (address.family === 'IPv4' && !address.internal) {
+               addresses.push(address.address);
+           }
+       }
+   }
+
+   // Respond with some HTML
+   var respHTML = 'Hello MongoDB.<br/>';
+   if (!mongoCreatedCollections) { respHTML = respHTML + ' - ERROR, could not connect to DB<br/>'; }
+   respHTML = respHTML +
+      'requesting source:'+requestip+'<br/>'+
+      'webservice addresses:'+addresses;
+   if (!mongoCreatedCollections) { res.send(respHTML); return; }
+   
+   // store this info
+   var stamp = getDateTime();
+   console.log('inserting from:' + requestip + ' served by node ' + addresses);
+   db.createCollection('history', {strict:false}, function(err, collection) {
+      collection.insert({date:stamp,theirIp:requestip,myIp:addresses}, function(err, records) { if (err) throw err;});
    });
-});
-app.get('/test2', function(req, res) {
-   if (!mongoCreatedCollections) { res.send('test2 - no DB connection'); return; }
-   db.collection('test2').find().toArray(function(err, items){
-      console.log(items);
-      res.send('items in test 2: ' + items.length);
+
+   respHTML = respHTML + '<hr>' + '<h3>GET Requests History</h3><ul>';
+   db.collection('history').find().toArray(function(err, items) {
+      for (var i = items.length - 1; i >= 0; i--) {
+         var itemout = '<li> date:' + items[i].date+', them:'+items[i].theirIp+', us:'+items[i].myIp+'</li>';
+         //console.log(itemout);
+         respHTML += itemout;
+      };
+      respHTML = respHTML + '</ul><br/> last updated:' + stamp;
+      res.send(respHTML);
    });
-});
-app.get('/test3', function(req, res) {
-   if (!mongoCreatedCollections) { res.send('test3 - no DB connection'); return; }
-   db.collection('test3').find().toArray(function(err, items){
-      console.log(items);
-      res.send('items in test 3: ' + items.length);
-   });
+   //respHTML = respHTML + '<br/> last updated:' + stamp;
+   //res.send(respHTML);
 });
 
 // setup MongoDB
@@ -71,3 +87,20 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 app.listen(server_port, server_ip_address, function() {
    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
+
+//------------------------------------------------
+function getDateTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
